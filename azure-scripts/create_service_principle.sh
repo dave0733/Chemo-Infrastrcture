fc1372405c8cff0af2ca6b99c1a3b513127ceaaf
“James <james.cordova@cancer.org>
2022-12-03 00:42:00 +0600

    added infra scripts

--- azure-scripts/create_service_principle.sh
+++
@@ -1,9 +0,0 @@
-#Create a service principal to access your resource group with the Contributor role using the following command,
-
-Name="spiga_github_sp"
-
-az ad sp create-for-rbac \
-    --name $Name \
-    --scope /subscriptions/e320cadf-67e8-4c2a-9a16-5db613904d7e/resourceGroups/spigaspaceshooter-rg \
-    --role Contributor \
-    --sdk-auth
